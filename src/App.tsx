04d7d0239040a379ae15a31c13197515d264d2d4
sinsinpurin <tusanch6@gmail.com>
2021-08-23 00:57:30 +0900

    add firebase configure and fix

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => {
       <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.feactHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
-      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
+      <Row title="Documentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
